3790987e2f81f2360e7dff20ad0e836bfd82cea6
idmyn <hello@davidmyno.rs>
2019-10-21 14:07:28 +0100

    add routes for log-in and sign-up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :ideas
-  resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, except: [:new, :create]
+  # post "ideas/:id/pin", to: "ideas#new_pin", as: "new_pin"
+  get "/log-in", to: "sessions#new", as: "new_session"
+  post "/log-in", to: "sessions#create"
+  get "/sign-up", to: "users#new", as: "new_user"
+  post "/sign-up", to: "users#create"
 end
