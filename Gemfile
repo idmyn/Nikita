c390a034b1e35d5444c7289ae3cac466e56d58eb
idmyn <hello@davidmyno.rs>
2019-10-21 14:06:37 +0100

    finish seeds.rb

--- Gemfile
+++ Gemfile
@@ -3,6 +3,10 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.6.1'
 
+gem "pry"
+gem "pry-doc"
+gem "faker"
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.0'
 # Use sqlite3 as the database for Active Record
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-
 require "faker"
-
-def user
-  email = Faker::Internet.email
-  name = Faker::FunnyName.name
-  password = Faker::Internet.password
-  email + " " + name + " " + password
+10.times do
+  User.create({
+    email: Faker::Internet.email,
+    name: Faker::FunnyName.name,
+    password_digest: Faker::Internet.password
+  })
 end
 
-def idea
-  title = Faker::Hipster.sentence
-  body = Faker::Lorem.paragraphs(number: 3).join(" ")
-  title + " " + body
+User.all.each do |user|
+  2.times do
+    user.ideas.create({
+      title: Faker::Hipster.sentence,
+      body: Faker::Lorem.paragraphs(number: 3).join(" ")
+    })
+  end
 end
