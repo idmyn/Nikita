b19957a70bcc7ac2b0a44980abf7a42b5b4f8595
idmyn <hello@davidmyno.rs>
2019-10-25 14:11:37 +0100

    finished mvp!!

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,9 @@
 require "faker"
+
+User.destroy_all
+Idea.destroy_all
+Pin.destroy_all
+
 10.times do
   User.create({
     email: Faker::Internet.email,
@@ -8,10 +13,14 @@ require "faker"
 end
 
 User.all.each do |user|
-  2.times do
+  7.times do
     user.ideas.create({
       title: Faker::Hipster.sentence,
       body: Faker::Lorem.paragraphs(number: 3).join(" ")
     })
+  end
+
+  4.times do
+    Idea.all.sample.pins.create(user_id: user.id)
   end
 end
