121e7d69e97a3ffec5646a8ccf5fe03b4583a70e
idmyn <hello@davidmyno.rs>
2019-10-25 10:35:10 +0100

    abstract idea index column pair logic to idea model

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -7,6 +7,17 @@ class Idea < ApplicationRecord
     validates :title, length: {maximum: 75}
     validates :body, length: {maximum: 300}
 
+    def self.idea_pairs
+      all_ideas = self.all.reverse
+      i = 0
+      result = []
+      until i >= all_ideas.length
+        result.push([all_ideas[i], all_ideas[i + 1]])
+        i += 2
+      end
+      result
+    end
+
     def pinned_by?(user)
         pins.find_by(user_id: user.id)
     end
@@ -14,6 +25,4 @@ class Idea < ApplicationRecord
     def pins_count
         pins.count == 1 ? "Pinned #{pins.count} time" : "Pinned #{pins.count} times"
     end
-
-    
 end
